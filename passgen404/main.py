9c287fb2bbb22c6f841b5e4b3125578efa766fe1
b4dcat404 <rmonek@yandex.ru>
2022-11-29 18:25:54 +0300

    create simple generation random passwords 3 in 1 message

--- passgen404/main.py
+++ passgen404/main.py
@@ -1,8 +1,24 @@
 # Telegram Bot Password Generator
-
+#
+# *** IMPORTS ***
 import telebot
-from password_generator import PasswordGenerator
+import passgen
 import config
 
-pwo = PasswordGenerator()
+# *** PARAMS ***
+pwo = passgen.PasswordGenerator()
 bot = telebot.TeleBot(config.token)
+
+# *** BOT ***
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Hi, I can create many random passwords for you.\nType /gen to create 3 random "
+                                      "passwords.")
+
+@bot.message_handler(commands=["gen"])
+def gen(message):
+    bot.send_message(message.chat.id, '`' + pwo.generate() + '`' + '\n' + '`' + pwo.generate() + '`' + '\n' + '`' + pwo.generate() + '`' + '\n', parse_mode="MarkdownV2")
+
+
+
+bot.polling(none_stop=True, interval=0)
